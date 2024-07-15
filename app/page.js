7f63f7fe79b48e26286b313fcc16528d79997f4b
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-07-15 16:35:04 +0330

    creating products menu

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
 import Features from "@/components/Features";
+import ProductsTab from "@/components/products/ProductsTab";
+import { getFetch } from "@/utils/fetch";
 
 
-export default function Home() {
+export default async function Home() {
+
+  const productsTab= await getFetch('/products/products-tabs');
+  console.log(productsTab)
   return (
-  
+  <>
     <Features />
+
+    <ProductsTab  tabList={productsTab.tabList} tabPanel={productsTab.tabPanel}/>
+
+    </>
   );
 }
