a1d160853ddbeb4a2ffa56efa0080aa839101b91
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-08-12 16:08:34 +0330

    Adding progress bar and spinner to project

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import BootstrapClient from "@/components/libraries/Bootstrap";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Toastify from "@/components/libraries/Toastify";
+import Footer from "@/components/layout/Footer";
+import NextNprogress from "@/components/libraries/NextNprogress";
 
 
 
@@ -10,11 +12,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body >
-
+      <NextNprogress >
         <Header />
         {children}
+        <Footer />
         <BootstrapClient />
         <Toastify />
+        </NextNprogress>
 
       </body>
     </html>
